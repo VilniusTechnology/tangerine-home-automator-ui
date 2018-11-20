65e53d1418189b7ccf2e5ffdd054b476623c6a54
Lukas Mikelionis <lmikelionis@gmail.com>
2018-11-20 18:01:56 +0200

    Added promises

---
+++ src/app/promises/promises.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promises',
+  templateUrl: './promises.component.html',
+  styleUrls: ['./promises.component.scss']
+})
+export class PromisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  private performUpload() {
+    
+  }
+}
